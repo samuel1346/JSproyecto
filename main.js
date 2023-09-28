23db308829bce906648174229fd1218e1005c5ba
samuel1346 <samielnsilva@gmail.com>
2023-09-28 18:02:45 -0300

    primer entrega

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-const convertAndReverse = 0;
-const returnReverseValue = number => {
-   const convertAndReverse = number.toString().split("").reverse().join("");
-   return Number(convertAndReverse);
-};
-let number = Number(prompt("elige un numero de tres digitos ordenados de mayor a menor"));
+alert("El juego se basa en elegir un número del 1 al 10. Si tu número es mayor que el de la computadora, ganas.");
 
-if(number>0){
-   alert("ahora restale el mismo numero pero invertido");
-   alert(`acaso el resultado es ${number - returnReverseValue(number)}?`);
-}
-else{
-   alert("algo hicimos mal")
+let wins = 0;
+let losses = 0;
+
+const getUserNumber = () => {
+  const userNumber = parseInt(prompt("Elige un número del 1 al 10"));
+  if (userNumber < 1 || userNumber > 10 || isNaN(userNumber)) {
+    alert("Por favor, ingresa un número válido del 1 al 10.");
+    return getUserNumber();
+  }
+  return userNumber;
 };
+
+const playAgain = () => {
+  const response = prompt("¿Quieres jugar de nuevo? (si/no)").toLowerCase();
+  return response === "si";
+};
+
+while (true) {
+  const userNumber = getUserNumber();
+  const playerPc = Math.floor(Math.random() * 10) + 1;
+
+  if (userNumber > playerPc) {
+    alert("¡Ganaste!");
+    wins++;
+  } else if (userNumber < playerPc) {
+    alert("Perdiste.");
+    losses++;
+  } else {
+    alert("Es un empate.");
+  }
+
+  if (!playAgain()) {
+    break;
+  }
+}
+
+alert(`Juegos ganados: ${wins} Juegos perdidos: ${losses}`);
